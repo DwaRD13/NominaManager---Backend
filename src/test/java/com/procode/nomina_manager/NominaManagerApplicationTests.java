a573f70fa338d7066cc5615ea68d5784021af8b9
DwaRD13 <munozdarwin013@gmail.com>
2026-03-03 16:31:16 -0400

    Primer commit

---
+++ src/test/java/com/procode/nomina_manager/NominaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procode.nomina_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NominaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
